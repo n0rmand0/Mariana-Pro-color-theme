0f73ab63e4900a85bf592c21f598a5a54352b6e3
Cheng Lou <chenglou92@gmail.com>
2021-02-08 01:37:18 -0800

    Color keyword
    Follow-up of https://github.com/n0rmand0/Mariana-Pro-color-theme/issues/9#issuecomment-774649886
    
    The scope exclusion `-` syntax doesn't work in VSCode and is actually invalid syntax, so that caused keywords not to be highlighted. I've simply removed that syntax.
    
    This doesn't actually change `keyword.operator`'s existing behavior! No color breaks. `keyword.operator` was already specifically highlighted in https://github.com/n0rmand0/Mariana-Pro-color-theme/blob/4f8900906996730c57f29bcd667053a9b5149264/src/MarianaPro-color-theme.js#L348-L350
    
    Aka this change re-enables `keyword` coloring, and doesn't disturb `keyword.operator` existing color. Win-win.

--- src/MarianaPro-color-theme.js
+++ src/MarianaPro-color-theme.js
@@ -326,7 +326,7 @@ module.exports = {
     {
       name: "Keyword",
       scope: [
-        "keyword - keyword.operator",
+        "keyword",
         "keyword.operator.word",
         "keyword.control",
         "keyword.operator.new.js",
